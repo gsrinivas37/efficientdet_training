2fc6355ac1dd949e81cecdd0fad371988de92847
sgudla9 <Stowe@1234>
2023-04-24 17:45:55 -0400

    Revert generate tfrecord

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -4,11 +4,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-import hashlib
 import os
 import io
-
-import PIL
 import pandas as pd
 
 from tensorflow.python.framework.versions import VERSION
@@ -51,12 +48,15 @@ def split(df, group):
     return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
 
 
-def create_tf_example(id, group, path):
-    img_raw = open(path, "rb").read()
-    key = hashlib.sha256(img_raw).hexdigest()
-    filename = os.path.basename(path)
-    width, height = PIL.Image.open(path).size
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
 
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
     xmins = []
     xmaxs = []
     ymins = []
@@ -73,41 +73,30 @@ def create_tf_example(id, group, path):
         classes.append(class_text_to_int(row['class']))
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
-        "image/height": tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
-        "image/width": tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
-        "image/filename": tf.train.Feature(bytes_list=tf.train.BytesList(value=[filename.encode("utf-8")])),
-        "image/source_id": tf.train.Feature(bytes_list=tf.train.BytesList(value=[str(id).encode("utf-8")])),
-        "image/key/sha256": tf.train.Feature(bytes_list=tf.train.BytesList(value=[key.encode("utf-8")])),
-        "image/encoded": tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw])),
-        "image/format": tf.train.Feature(bytes_list=tf.train.BytesList(value=["jpg".encode("utf8")])),
-        "image/object/bbox/xmin": tf.train.Feature(
-            float_list=tf.train.FloatList(value=xmins)),
-        "image/object/bbox/xmax": tf.train.Feature(
-            float_list=tf.train.FloatList(value=xmaxs)),
-        "image/object/bbox/ymin": tf.train.Feature(
-            float_list=tf.train.FloatList(value=ymins)),
-        "image/object/bbox/ymax": tf.train.Feature(
-            float_list=tf.train.FloatList(value=ymaxs)),
-        "image/object/class/text": tf.train.Feature(
-            bytes_list=tf.train.BytesList(value=["license_plate".encode("utf-8")])),
-        "image/object/class/label": tf.train.Feature(int64_list=tf.train.Int64List(value=classes)),
-        "image/object/difficult": tf.train.Feature(int64_list=tf.train.Int64List(value=[0])),
-        "image/object/truncated": tf.train.Feature(int64_list=tf.train.Int64List(value=[0])),
-        "image/object/view": tf.train.Feature(bytes_list=tf.train.BytesList(value=["Unspecified".encode("utf-8")])),
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
     }))
     return tf_example
 
 
 def generate_tf_records(csv_input, output_path, image_dir):
     writer = tf.python_io.TFRecordWriter(output_path)
+    path = os.path.join(image_dir)
     examples = pd.read_csv(csv_input)
     grouped = split(examples, 'filename')
-    i = 0
     for group in grouped:
-        path = os.path.join(image_dir, group.filename)
-        tf_example = create_tf_example(i, group, path)
+        tf_example = create_tf_example(group, path)
         writer.write(tf_example.SerializeToString())
-        i = i + 1
 
     writer.close()
     output_path = os.path.join(os.getcwd(), output_path)
